8609053f3cb3dc6c26fd173d6fca8391e1b36594
shabankasumi <shaban.kasumi07@gmail.com>
2025-12-07 20:53:20 +0100

    Initial commit - remove .vs folder

---
+++ Local Service Manager/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Local_Service_Manager.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Local_Service_Manager.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
